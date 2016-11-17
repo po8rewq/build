d72b12e3d63045abeba20af831b8ae1aa3cb75c5
Adrien Fischer <adrien.fischer@massive.co>
2016-11-17 14:46:24 +0000

    added a version tag, in order to use a specific cordova platform

--- build/Cordova.hx
+++ build/Cordova.hx
@@ -36,6 +36,10 @@ class Cordova
 	{
 		var refresh = config.getValue('define.refreshPlugin', 'none');
 		var platform = config.getValue('cordova.platform');
+
+		var version = config.getValue('cordova.version');
+		if(version != null) platform += '@$version';
+		
 		var path = config.getValue('cordova.path');
 		var pluginConfigs = config.getValue('cordova.plugins', new Array<OrderedMap>());
 		var plugins = pluginConfigs.map(function(plugin){
